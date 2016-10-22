e7a39205bc5e0d167fe2b523f8c7eed1fc687751
Mauricio Guignard <mauguignard@gmail.com>
2016-10-22 16:47:41 -0300

    Implemented onError and onSuccess methods. Removed old commented code

--- app/src/main/java/ar/edu/unc/famaf/redditreader/backend/Backend.java
+++ app/src/main/java/ar/edu/unc/famaf/redditreader/backend/Backend.java
@@ -38,12 +38,17 @@ public class Backend {
         new GetTopPostsTask() {
             @Override
             void onError() {
-                /* TODO */
+                /* Trigger swipeContainer.setRefreshing(false); */
+                if (adapter != null)
+                    adapter.notifyDataSetChanged();
             }
 
             @Override
             void onSuccess(Listing result) {
-                /* TODO */
+                mLstPostsModel.clear();
+                mLstPostsModel.addAll(result.getLstPostsModel());
+                if (adapter != null)
+                    adapter.notifyDataSetChanged();
             }
         };
     }
@@ -57,62 +62,18 @@ public class Backend {
         new GetTopPostsTask(LIMIT, after) {
             @Override
             void onError() {
-                /* TODO */
+                /* Trigger swipeContainer.setRefreshing(false); */
+                if (adapter != null)
+                    adapter.notifyDataSetChanged();
             }
 
             @Override
             void onSuccess(Listing result) {
-                /* TODO */
+                mLstPostsModel.addAll(result.getLstPostsModel());
+                if (adapter != null)
+                    adapter.notifyDataSetChanged();
             }
         };
     }
-
-    /*
-    private void getTopPosts(final boolean append) {
-        String after = null;
-
-        if (append && !mLstPostsModel.isEmpty())
-            after = mLstPostsModel.get(mLstPostsModel.size() - 1).getName();
-
-        JSONDownloadTask downloader = new JSONDownloadTask() {
-            @Override
-            protected void onPostExecute(JSONObject result) {
-                try {
-                    JSONObject data = result.getJSONObject("data");
-                    JSONArray children = data.getJSONArray("children");
-
-                    if (!append)
-                        mLstPostsModel.clear();
-
-                    for (int i = 0; i < children.length(); i++) {
-                        JSONObject child = children.getJSONObject(i);
-                        JSONObject childData = child.getJSONObject("data");
-
-                        PostModel item = new PostModel(childData);
-                        mLstPostsModel.add(item);
-
-                        if (adapter != null)
-                            adapter.notifyDataSetChanged();
-                    }
-
-                    Log.i("JSON", String.format("Read %1$d objects.", children.length()));
-                }catch (Exception e) {
-                    Log.e("JSON_ERROR", "Unable to parse JSON object");
-                    if (e.getMessage() != null) {
-                        Log.e("JSON_ERROR", e.getMessage());
-                    }
-                    e.printStackTrace();
-                }
-            }
-        };
-
-        String url = "https://www.reddit.com/top/.json?&limit=" + Integer.toString(LIMIT);
-
-        if (after != null)
-            url += "&after=" + after;
-
-        downloader.execute(url);
-    }
-    */
 
 }
