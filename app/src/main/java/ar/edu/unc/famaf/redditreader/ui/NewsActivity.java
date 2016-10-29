bc1f70ea9959a684afb950949dbe992499fb03e9
Mauricio Guignard <mauguignard@gmail.com>
2016-10-28 21:41:58 -0300

    Moved code from Activity to Fragment

--- app/src/main/java/ar/edu/unc/famaf/redditreader/ui/NewsActivity.java
+++ app/src/main/java/ar/edu/unc/famaf/redditreader/ui/NewsActivity.java
@@ -33,81 +33,6 @@ public class NewsActivity extends AppCompatActivity {
 
         // Initialize Bitmap Disk Cache
         BitmapCache.getInstance().initDiskCache(getApplicationContext());
-
-        final SwipeRefreshLayout swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
-
-        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                Backend.getInstance().getTopPosts();
-            }
-        });
-
-        swipeContainer.setColorSchemeResources(R.color.flairNoPress,
-                R.color.flairPress6,
-                R.color.flairPress5,
-                R.color.flairPress4,
-                R.color.flairPress3,
-                R.color.flairPress2,
-                R.color.flairPress1);
-
-
-        PostAdapter adapter = new PostAdapter(this, R.layout.post_row,
-                Backend.getInstance().getLst()) {
-            @Override
-            public void notifyDataSetChanged() {
-                super.notifyDataSetChanged();
-                swipeContainer.setRefreshing(false);
-            }
-        };
-
-        Backend.getInstance().setAdapter(adapter);
-
-        final ListView PostsLV = (ListView) findViewById(R.id.postsListView);
-
-
-        if (Backend.getInstance().getLst().isEmpty()) {
-            Backend.getInstance().getTopPosts();
-            swipeContainer.setRefreshing(true);
-        } else {
-            PostsLV.setLayoutAnimation(null);
-        }
-
-        // Add dummy footer to avoid crash before API 19
-        if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
-            PostsLV.addFooterView(new View(this.getBaseContext()));
-        }
-        PostsLV.setAdapter(adapter);
-
-        final LinearLayout progressBarFooter = (LinearLayout) getLayoutInflater().inflate(
-                R.layout.progress_bar_footer, null, false);
-
-        PostsLV.setOnScrollListener(new AbsListView.OnScrollListener() {
-            private static final int THRESHOLD = 5;
-            private int previousTotal = 0;
-            private boolean loading = true;
-
-            @Override
-            public void onScrollStateChanged(AbsListView view, int scrollState) {
-
-            }
-
-            @Override
-            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
-                                 int totalItemCount) {
-                if (loading) {
-                    if (Math.abs(totalItemCount - previousTotal) >= THRESHOLD) {
-                        loading = false;
-                        previousTotal = totalItemCount;
-                        PostsLV.removeFooterView(progressBarFooter);
-                    }
-                } else if (totalItemCount - visibleItemCount <= firstVisibleItem + THRESHOLD) {
-                    Backend.getInstance().getNextTopPosts();
-                    loading = true;
-                    PostsLV.addFooterView(progressBarFooter);
-                }
-            }
-        });
     }
 
     @Override
@@ -141,7 +66,6 @@ public class NewsActivity extends AppCompatActivity {
                 Log.i("LOGIN_SUCCESSFUL", login_message);
             } else {
                 Log.i("LOGIN_UNSUCCESSFUL", "No user logged yet");
-
             }
         }
     }
--- app/src/main/java/ar/edu/unc/famaf/redditreader/ui/NewsActivityFragment.java
+++ app/src/main/java/ar/edu/unc/famaf/redditreader/ui/NewsActivityFragment.java
@@ -1,12 +1,18 @@
 package ar.edu.unc.famaf.redditreader.ui;
 
+import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import ar.edu.unc.famaf.redditreader.R;
+import ar.edu.unc.famaf.redditreader.backend.Backend;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -19,6 +25,82 @@ public class NewsActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        View view = inflater.inflate(R.layout.fragment_news, container, false);
+
+        final SwipeRefreshLayout swipeContainer =
+                (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
+
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Backend.getInstance().getTopPosts();
+            }
+        });
+
+        swipeContainer.setColorSchemeResources(R.color.flairNoPress,
+                R.color.flairPress6,
+                R.color.flairPress5,
+                R.color.flairPress4,
+                R.color.flairPress3,
+                R.color.flairPress2,
+                R.color.flairPress1);
+
+        PostAdapter adapter = new PostAdapter(this.getContext(), R.layout.post_row,
+                Backend.getInstance().getLst()) {
+            @Override
+            public void notifyDataSetChanged() {
+                super.notifyDataSetChanged();
+                swipeContainer.setRefreshing(false);
+            }
+        };
+
+        Backend.getInstance().setAdapter(adapter);
+
+        final ListView PostsLV = (ListView) view.findViewById(R.id.postsListView);
+
+        if (Backend.getInstance().getLst().isEmpty()) {
+            Backend.getInstance().getTopPosts();
+            swipeContainer.setRefreshing(true);
+        } else {
+            PostsLV.setLayoutAnimation(null);
+        }
+
+        // Add dummy footer to avoid crash before API 19
+        if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            PostsLV.addFooterView(new View(this.getContext()));
+        }
+        PostsLV.setAdapter(adapter);
+
+        final LinearLayout progressBarFooter = (LinearLayout) getActivity().getLayoutInflater().
+                inflate(R.layout.progress_bar_footer, null, false);
+
+        PostsLV.setOnScrollListener(new AbsListView.OnScrollListener() {
+            private static final int THRESHOLD = 5;
+            private int previousTotal = 0;
+            private boolean loading = true;
+
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
+                                 int totalItemCount) {
+                if (loading) {
+                    if (Math.abs(totalItemCount - previousTotal) >= THRESHOLD) {
+                        loading = false;
+                        previousTotal = totalItemCount;
+                        PostsLV.removeFooterView(progressBarFooter);
+                    }
+                } else if (totalItemCount - visibleItemCount <= firstVisibleItem + THRESHOLD) {
+                    Backend.getInstance().getNextTopPosts();
+                    loading = true;
+                    PostsLV.addFooterView(progressBarFooter);
+                }
+            }
+        });
+
+        return view;
     }
 }
