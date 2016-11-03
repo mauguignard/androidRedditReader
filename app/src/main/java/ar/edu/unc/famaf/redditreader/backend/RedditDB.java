e829d5dc6759ebf654dfca38d0cef743fb2ad0df
Mauricio Guignard <mauguignard@gmail.com>
2016-11-03 19:47:45 -0300

    Added Database

---
+++ app/src/main/java/ar/edu/unc/famaf/redditreader/backend/RedditDB.java
@@ -0,0 +1,107 @@
+package ar.edu.unc.famaf.redditreader.backend;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unc.famaf.redditreader.BuildConfig;
+import ar.edu.unc.famaf.redditreader.model.Listing;
+import ar.edu.unc.famaf.redditreader.model.PostModel;
+
+/**
+ * Created by mauguignard on 10/28/16.
+ */
+
+class RedditDB {
+    public static Listing getPostsFromDB(Context context, int from, int limit) {
+        RedditDBHelper dbHelper = new RedditDBHelper(context, BuildConfig.VERSION_CODE);
+
+        List<PostModel> result = new ArrayList<>();
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery("SELECT * FROM " + RedditDBHelper.POST_TABLE, null);
+
+        // Loop throw all rows until limit
+        if (cursor.moveToPosition(from)) {
+            int i = 0;
+            while (!cursor.isAfterLast() && i < limit) {
+                PostModel post = new PostModel();
+                post.setDomain(cursor.getString(1));
+                post.setSubreddit(cursor.getString(2));
+                post.setGilded(cursor.getInt(3));
+                post.setAuthor(cursor.getString(4));
+                post.setName(cursor.getString(5));
+                post.setScore(cursor.getInt(6));
+                post.setOver18(cursor.getInt(7) != 0);
+                post.setThumbnail(cursor.getString(8));
+                post.setPermalink(cursor.getString(9));
+                post.setCreated(cursor.getLong(10));
+                post.setLinkFlairText(cursor.getString(11));
+                post.setURL(cursor.getString(12));
+                post.setTitle(cursor.getString(13));
+                post.setNoComments(cursor.getInt(14));
+                post.setDowns(cursor.getInt(15));
+                post.setUps(cursor.getInt(16));
+                result.add(post);
+                cursor.moveToNext();
+                i++;
+            }
+        }
+
+        cursor.close();
+
+        return new Listing(result, null, null);
+    }
+
+    public static void savePostsToDB(Context context, Listing listing) {
+        RedditDBHelper dbHelper = new RedditDBHelper(context, BuildConfig.VERSION_CODE);
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        // Use transaction to speed up multiple insertions
+        db.beginTransaction();
+
+        for (PostModel post : listing.getLstPostsModel()) {
+
+            ContentValues values = new ContentValues();
+            values.put(RedditDBHelper.POST_TABLE_DOMAIN, post.getDomain());
+            values.put(RedditDBHelper.POST_TABLE_SUBREDDIT, post.getSubreddit());
+            values.put(RedditDBHelper.POST_TABLE_GILDED, post.getGilded());
+            values.put(RedditDBHelper.POST_TABLE_AUTHOR, post.getAuthor());
+            values.put(RedditDBHelper.POST_TABLE_NAME, post.getName());
+            values.put(RedditDBHelper.POST_TABLE_SCORE, post.getScore());
+            values.put(RedditDBHelper.POST_TABLE_OVER18, post.isOver18() ? 1 : 0);
+            values.put(RedditDBHelper.POST_TABLE_THUMBNAIL, post.getThumbnail());
+            values.put(RedditDBHelper.POST_TABLE_PERMALINK, post.getPermalink());
+            values.put(RedditDBHelper.POST_TABLE_CREATED, post.getCreated());
+            values.put(RedditDBHelper.POST_TABLE_LINK_FLAIR_TEXT, post.getLinkFlairText());
+            values.put(RedditDBHelper.POST_TABLE_URL, post.getURL());
+            values.put(RedditDBHelper.POST_TABLE_TITLE, post.getTitle());
+            values.put(RedditDBHelper.POST_TABLE_NO_COMMENTS, post.getNoComments());
+            values.put(RedditDBHelper.POST_TABLE_DOWNS, post.getDowns());
+            values.put(RedditDBHelper.POST_TABLE_UPS, post.getUps());
+
+            db.insert(RedditDBHelper.POST_TABLE, null, values);
+        }
+
+        /* Remove old Posts (Limit to the last mMaxPosts)
+        String delQuery = "DELETE FROM " + RedditDBHelper.POST_TABLE + " WHERE "
+                + RedditDBHelper.POST_TABLE_ID + " NOT IN ("
+                + "SELECT " + RedditDBHelper.POST_TABLE_ID + " FROM " + RedditDBHelper.POST_TABLE
+                + " ORDER BY " + RedditDBHelper.POST_TABLE_ID + " DESC LIMIT " + mMaxPosts + ");";*/
+
+        String delQuery = "DELETE FROM " + RedditDBHelper.POST_TABLE + ";";
+
+        Log.i("SQLITE", "Executed: " + delQuery);
+        db.rawQuery(delQuery, null);
+
+        db.setTransactionSuccessful();
+        db.endTransaction();
+    }
+}
---
+++ app/src/main/java/ar/edu/unc/famaf/redditreader/backend/RedditDBHelper.java
@@ -0,0 +1,76 @@
+package ar.edu.unc.famaf.redditreader.backend;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by mauguignard on 10/27/16.
+ */
+
+class RedditDBHelper extends SQLiteOpenHelper {
+    private static final String LOG_TAG = "RedditDBHelper";
+
+    private static final String DATABASE_NAME = "redder.db";
+
+    static final String POST_TABLE = "post";
+
+    private static final String POST_TABLE_ID = "_id";
+
+    static final String POST_TABLE_DOMAIN = "domain";
+    static final String POST_TABLE_SUBREDDIT = "subreddit";
+    static final String POST_TABLE_GILDED = "gilded";
+    static final String POST_TABLE_AUTHOR = "author";
+    static final String POST_TABLE_NAME = "name";
+    static final String POST_TABLE_SCORE= "score";
+    static final String POST_TABLE_OVER18 = "over18";
+    static final String POST_TABLE_THUMBNAIL = "thumbnail";
+    static final String POST_TABLE_PERMALINK = "permalink";
+    static final String POST_TABLE_CREATED = "created";
+    static final String POST_TABLE_LINK_FLAIR_TEXT = "link_flair_text";
+    static final String POST_TABLE_URL = "url";
+    static final String POST_TABLE_TITLE = "title";
+    static final String POST_TABLE_NO_COMMENTS = "no_comments";
+    static final String POST_TABLE_DOWNS = "downs";
+    static final String POST_TABLE_UPS = "ups";
+
+
+    RedditDBHelper(Context context, int version) {
+        super(context, DATABASE_NAME, null, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createSentence =
+                "CREATE TABLE " + POST_TABLE + " ("
+                + POST_TABLE_ID + " integer primary key autoincrement,"
+                + POST_TABLE_DOMAIN + " text not null,"
+                + POST_TABLE_SUBREDDIT + " text not null,"
+                + POST_TABLE_GILDED + " integer,"
+                + POST_TABLE_AUTHOR + " text not null,"
+                + POST_TABLE_NAME + " text not null,"
+                + POST_TABLE_SCORE + " integer,"
+                + POST_TABLE_OVER18 + " integer,"
+                + POST_TABLE_THUMBNAIL + " text not null,"
+                + POST_TABLE_PERMALINK + " text not null,"
+                + POST_TABLE_CREATED + " integer,"
+                + POST_TABLE_LINK_FLAIR_TEXT + " text,"
+                + POST_TABLE_URL + " text not null,"
+                + POST_TABLE_TITLE + " text not null,"
+                + POST_TABLE_NO_COMMENTS + " integer,"
+                + POST_TABLE_DOWNS + " integer,"
+                + POST_TABLE_UPS + " integer" + ")";
+        db.execSQL(createSentence);
+
+        Log.i(LOG_TAG, "Database created.");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + POST_TABLE);
+        this.onCreate(db);
+
+        Log.i(LOG_TAG, "Database updated");
+    }
+}
