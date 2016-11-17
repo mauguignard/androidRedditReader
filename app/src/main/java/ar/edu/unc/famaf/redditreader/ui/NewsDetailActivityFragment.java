86804ae829cc942e35a4a557dac72fefe3316f4b
Mauricio Guignard <mauguignard@gmail.com>
2016-11-17 13:20:04 -0300

    Updated dependencies

---
+++ app/src/main/java/ar/edu/unc/famaf/redditreader/ui/NewsDetailActivityFragment.java
@@ -0,0 +1,23 @@
+package ar.edu.unc.famaf.redditreader.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ar.edu.unc.famaf.redditreader.R;
+
+public class NewsDetailActivityFragment extends Fragment {
+    public NewsDetailActivityFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news_detail, container, false);
+    }
+}
